971937e88f0d8b0d9348092d9669196dc95910b9
Nishant27Saini <50879814+Nishant27Saini@users.noreply.github.com>
2021-06-18 18:03:29 +0530

    BFS Traversal on Graph

---
+++ BFS.py
@@ -0,0 +1,29 @@
+class Graph:
+    def __init__(self, gdict=None):
+        self.gdict = gdict
+    def addconnection(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+    def countconnection(self,node):
+        return len(self.gdict[node])
+    def addNode(self,newnode):
+        self.gdict[newnode] = []
+    # traversal
+    def showNode(self):
+        return self.gdict.keys()
+    def showValues(self):
+        return self.gdict.values()
+    def bfs(self,vertex):
+        if vertex not in self.gdict:
+            print("not found :",vertex, "in", self.gdict.keys())
+        else:
+            queue = [ vertex ]
+            visited = [ vertex ]
+            while queue:
+                deVertex = queue.pop(0)
+                print(deVertex)
+                for adjacentVertex in self.gdict[deVertex]:
+                    if adjacentVertex not in visited:
+                        # enqueue add into queue
+                        queue.append(adjacentVertex)
+                        # mark it visited
+                        visited.append(adjacentVertex)
